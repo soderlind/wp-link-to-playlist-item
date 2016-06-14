c058bb16ad2c78ce5d282c500d1e693638f21a7c
soderlind <per@soderlind.no>
2016-06-14 18:47:20 +0200

    replaced onload event with custom event 'scroll:top'

--- js/wp-link-to-playlist-item.js
+++ js/wp-link-to-playlist-item.js
@@ -7,7 +7,7 @@
  * - Scrolls to top of media element (video or audio)
  *
  * @author Per Soderlind - https://soderlind.no
- * @version 0.2.5
+ * @version 0.2.6
  * @license: GPL2 (https://www.gnu.org/licenses/gpl-2.0.html)
  */
 
@@ -44,17 +44,17 @@ jQuery(function ($) {
 			wp_link_to_playlist_scrollto = $('.wp-playlist').offset().top - 20;
 		}
 
-		//when page is loaded or reloaded, scroll to top of wp_link_to_playlist_scrollto
-		$(window).on('load', function() {
-			$(window).scrollTop(wp_link_to_playlist_scrollto);
-		});
-		$(window).on('beforeunload', function() {
+		//custom event 'scroll:top' triggers scroll to top of wp_link_to_playlist_scrollto
+		$(document).on( 'scroll:top', function() {
 			$(window).scrollTop(wp_link_to_playlist_scrollto);
 		});
 
 		//play linked to item
 		if (wp_link_to_playlist_identifier && $('.wp-playlist-item').get().length && wp_link_to_playlist_identifier.slice(2) - 1 <= $('.wp-playlist-item').get().length) {
 			var wp_link_to_playlist_num = (wp_link_to_playlist_identifier.slice(2) - 1 < 0) ? 0 : wp_link_to_playlist_identifier.slice(2) - 1;
+			//scroll to top of wp_link_to_playlist_scrollto
+			$( document ).trigger( 'scroll:top' );
+			//play item.
 			wp_link_to_playlist_play_item($('.wp-playlist-item'),wp_link_to_playlist_num);
 		}
 
@@ -79,7 +79,7 @@ jQuery(function ($) {
 			wp_link_to_playlist_update_history_hash(newhash);
 
 			//scroll to top of wp_link_to_playlist_scrollto
-			$(window).scrollTop(wp_link_to_playlist_scrollto);
+			$( document ).trigger( 'scroll:top' );
 		});
 	}
 });
